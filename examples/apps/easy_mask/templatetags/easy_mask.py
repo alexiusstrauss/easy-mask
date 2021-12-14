5aacce133cbbc2f179cef4f73d90adf067046fc1
Sourcery AI <>
2021-12-14 20:01:06 +0000

    'Refactored by Sourcery'

--- examples/apps/easy_mask/templatetags/easy_mask.py
+++ examples/apps/easy_mask/templatetags/easy_mask.py
@@ -7,29 +7,21 @@ register = template.Library()
 @register.filter(name='phone')
 def phone_mask(item, mask="({}) {}-{}"):
     """ return (00) 00000-0000 or (00) 0000-0000  """
-    if item and len(item) >=11:
-        return mask.format(
-            item[0:2],
-            item[2:7],
-            item[7:11]
-        )
-    elif item:
-        return mask.format(
-            item[0:2],
-            item[2:6],
-            item[6:10]
-        )
+    if item:
+        if len(item) >= 11:
+            return mask.format(
+                item[0:2],
+                item[2:7],
+                item[7:11]
+            )
+        else:
+            return mask.format(item[:2], item[2:6], item[6:10])
 
 @register.filter(name='cpf')
 def cpf_mask(item, mask="{}.{}.{}-{}"):
     """ return 000.000.000-00 """
     if item:
-        return mask.format(
-            item[0:3],
-            item[3:6],
-            item[6:9],
-            item[9:11]
-        )
+        return mask.format(item[:3], item[3:6], item[6:9], item[9:11])
     else:
         return None
 
@@ -40,30 +32,15 @@ def rg_mask(item, mask="{}.{}.{}-{}", other_mask="{}.{}.{}"):
     in this case I was have to create another mask to work with in both objects
     """
     if item and len(item) >=8:
-        return mask.format(
-            item[0:2],
-            item[2:5],
-            item[5:8],
-            item[8:9]
-        )
+        return mask.format(item[:2], item[2:5], item[5:8], item[8:9])
     else:
-        return other_mask.format(
-            item[0:1],
-            item[1:4],
-            item[4:7]
-        )
+        return other_mask.format(item[:1], item[1:4], item[4:7])
 
 @register.filter(name='cnpj')
 def cnpj_mask(item, mask="{}.{}.{}/{}-{}"):
     """ return 00.000.000/0000-00 """
     if item:
-        return mask.format(
-            item[0:2],
-            item[2:5],
-            item[5:8],
-            item[8:12],
-            item[12:14]
-        )
+        return mask.format(item[:2], item[2:5], item[5:8], item[8:12], item[12:14])
     else:
         return None
 
@@ -71,10 +48,7 @@ def cnpj_mask(item, mask="{}.{}.{}/{}-{}"):
 def cep_mask(item, mask="{}-{}"):
     """ Return 00000-000 """
     if item:
-        return mask.format(
-            item[0:5],
-            item[5:8]
-    )
+        return mask.format(item[:5], item[5:8])
     else:
         return None
 
@@ -94,17 +68,6 @@ def ncm_mask(item, mask="{}.{}.{}"):
 def isbn_mask(item, mask="{}-{}-{}-{}-{}", other_mask="{}-{}-{}-{}"):
     """ Return 0-000-00000-0 or 000-00-000-0000-0 """
     if item and len(item) >=13:
-        return mask.format(
-            item[0:3],
-            item[3:5],
-            item[5:8],
-            item[8:12],
-            item[12:13]
-        )
+        return mask.format(item[:3], item[3:5], item[5:8], item[8:12], item[12:13])
     else:
-        return other_mask.format(
-            item[0:1],
-            item[1:4],
-            item[4:9],
-            item[9:10]
-        )
+        return other_mask.format(item[:1], item[1:4], item[4:9], item[9:10])
